aa8bc3b5841171dad5cf1c7d844874a38c37d69c
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-08 16:06:31 +0300

    Build Genaric Repository Pattern

---
+++ Threads.DataAccessLayer/Repository/Repository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Threads.DataAccessLayer.Data;
+using Threads.DataAccessLayer.RepositoryContracts;
+
+namespace Threads.DataAccessLayer.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet = db.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+        }
+
+        public async Task<T?> GetAsync(Expression<Func<T, bool>> filter, string? includeProperties, bool tracked = false)
+        {
+            IQueryable<T> query;
+
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
+
+            query = query.Where(filter);
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var property in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var property in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+            return await query.ToListAsync();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
---
+++ Threads.DataAccessLayer/RepositoryContracts/IRepository.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.Linq.Expressions;
+
+namespace Threads.DataAccessLayer.RepositoryContracts
+{
+    public interface IRepository<T> where T : class
+    {
+        // Get all entities asynchronously
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+
+        // Get a single entity asynchronously with filtering
+        Task<T?> GetAsync(Expression<Func<T, bool>> filter, string? includeProperties, bool tracked = false);
+
+        // Add an entity asynchronously
+        Task AddAsync(T entity); 
+
+        // Remove an entity
+        void Remove(T entity);
+
+        // Remove multiple entities
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
