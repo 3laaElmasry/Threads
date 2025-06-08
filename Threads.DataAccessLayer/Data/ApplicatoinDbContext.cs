cd218a087f5255ad794ecdc63fcc418bcd95b32f
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-08 15:43:51 +0300

    Init Migration

--- Threads.DataAccessLayer/Data/ApplicatoinDbContext.cs
+++ Threads.DataAccessLayer/Data/ApplicatoinDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 using Threads.DataAccessLayer.Data.Entities;
 using Threads.DataAccessLayer.Data1.Entities;
 
@@ -15,10 +16,66 @@ namespace Threads.DataAccessLayer.Data
 
         public DbSet<Comment> Comments { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Configure the Post entity
+            modelBuilder.Entity<Post>()
+                .HasKey(p => p.PostId);
 
-            base.OnModelCreating(builder);
+            modelBuilder.Entity<Post>()
+                .Property(p => p.Text)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            modelBuilder.Entity<Post>()
+                .Property(p => p.CreatedDate)
+                .IsRequired();
+
+            // Define relationship: Post belongs to an Author (User)
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Author)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction); // Prevent cascade delete from User to Post
+
+            // Define relationship: Post has many Comments
+            modelBuilder.Entity<Post>()
+                .HasMany(p => p.Comments)
+                .WithOne(c => c.Post)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.NoAction); // Prevent cascade delete from Post to Comment
+
+            // Configure Comment entity
+            modelBuilder.Entity<Comment>()
+                .HasKey(c => c.CommentId);
+
+            modelBuilder.Entity<Comment>()
+                .Property(c => c.Text)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            modelBuilder.Entity<Comment>()
+                .Property(c => c.CreatedDate)
+                .IsRequired();
+
+            modelBuilder.Entity<Comment>()
+                .Property(c => c.UpdatedDate);
+
+            // Define relationship: Comment belongs to an Author (User)
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Author)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction); // Prevent cascade delete from User to Comment
+
+            // Define relationship: Comment belongs to a Post
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.NoAction); // Ensure no cascade delete
+
+            base.OnModelCreating(modelBuilder);
         }
 
     }
--- Threads.DataAccessLayer/Data/Entities/Post.cs
+++ Threads.DataAccessLayer/Data/Entities/Post.cs
@@ -3,17 +3,17 @@
 
 namespace Threads.DataAccessLayer.Data1.Entities
 {
-    public class Post
-    {
-        public Guid PostId { get; set; }
-        public string Text { get; set; } = string.Empty;
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public class Post
+        {
+            public Guid PostId { get; set; }
+            public string Text { get; set; } = string.Empty;
+            public DateTime CreatedDate { get; set; }
+            public DateTime UpdatedDate { get; set; }
 
-        public Guid AuthorId { get; set; }
-        public ApplicationUser? Author { get; set; }
+            public Guid AuthorId { get; set; }
+            public ApplicationUser? Author { get; set; }
 
-        [ValidateNever]
-        public List<Comment>? Comments { get; set; }
-    }
+            [ValidateNever]
+            public List<Comment>? Comments { get; set; }
+        }
 }
