69d73734ff77825866eb3dcc41093d7819155174
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-08 13:45:53 +0300

    Enaple Swagger & Application Db Context

--- Threads.Api/Program.cs
+++ Threads.Api/Program.cs
@@ -1,23 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Threads.DataAccessLayer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // 👈 Enables Swagger
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(); // 👈 Enables the Swagger UI
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
---
+++ Threads.DataAccessLayer/Data/ApplicatoinDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Threads.DataAccessLayer.Data1.Entities;
+
+namespace Threads.DataAccessLayer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
