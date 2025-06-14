707b69464773400d9a642f713147db7594f6bd32
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-14 09:54:37 +0300

    Comment Repository

---
+++ Threads.DataAccessLayer/Repository/CommentRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Threads.DataAccessLayer.Data;
+using Threads.DataAccessLayer.Data.Entities;
+using Threads.DataAccessLayer.RepositoryContracts;
+
+namespace Threads.DataAccessLayer.Repository
+{
+    public class CommentRepository : Repository<Comment>, ICommentRepository
+    {
+        public CommentRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
