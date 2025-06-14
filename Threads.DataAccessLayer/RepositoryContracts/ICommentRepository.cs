795fe12146c86336f11c2ba080ce5f02d693b8d2
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-14 09:52:50 +0300

    I Commnet Repository interface

---
+++ Threads.DataAccessLayer/RepositoryContracts/ICommentRepository.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Threads.DataAccessLayer.Data.Entities;
+
+namespace Threads.DataAccessLayer.RepositoryContracts
+{
+    public interface ICommentRepository : IRepository<Comment>
+    {
+    }
+}
