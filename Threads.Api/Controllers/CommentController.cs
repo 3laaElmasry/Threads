a5c64bb2e4f4304829ebfe2c438f03d6f8d28214
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-14 10:35:24 +0300

    Post Request DTO

---
+++ Threads.Api/Controllers/CommentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Threads.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    //[Authorize]
+    public class CommentController : ControllerBase
+    {
+
+    }
+}
---
+++ Threads.BusinessLogicLayer/DTO/CommentDTO/CommentRequest.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Threads.BusinessLogicLayer.DTO.CommentDTO
+{
+    public class CommentRequest
+    {
+        [Required(ErrorMessage = "The Comment cann't be null or empty")]
+        public string? Text { get; set; }
+
+        [Required(ErrorMessage = "Post Id is Required to Publish a Comment")]
+        public string? PostId { get; set; }
+
+        public string? ParentId { get; set; }
+    }
+}
