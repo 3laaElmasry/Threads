dabcf1af5c1de53f2669614e7414e72694552eae
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-15 12:28:09 +0300

    Auth Model

---
+++ Threads.BusinessLogicLayer/Models/AuthModel.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Threads.BusinessLogicLayer.Models
+{
+    public class AuthModel
+    {
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsAuthonticated { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public List<string> Roles { get; set; } = new List<string>();
+
+        public DateTime ExpiresOn {  get; set; } 
+    }
+}
