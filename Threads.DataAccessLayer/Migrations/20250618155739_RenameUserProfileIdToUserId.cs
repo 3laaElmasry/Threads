de87c4a99c6781267e9fbb6ea59580cea6469e5d
Elmasry <3laaelmasry2005a@gmail.com>
2025-06-18 18:58:15 +0300

    Rename UserProfileId

---
+++ Threads.DataAccessLayer/Migrations/20250618155739_RenameUserProfileIdToUserId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Threads.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUserProfileIdToUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ClerkUserId",
+                table: "Users",
+                newName: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "ClerkUserId");
+        }
+    }
+}
